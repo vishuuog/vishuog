3bab1b270c7ffb70d7d486c7c4cc49bf09549117
vishuuog <vishvasgupta777@gmail.com>
2024-08-29 13:13:46 +0530

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,46 @@
+const html = document.documentElement;
+const canvas = document.getElementById("canvas");
+const context = canvas.getContext("2d");
+
+let framecount = 383;
+let currentframe = index => (
+    `./trauma/frame_${index.toString().padStart(4 , "0" )}.png`
+);
+
+const preloadimages = () =>{
+    for(i=1 ; i<framecount;  i++){
+        const img = new Image();
+        img.src = currentframe(i);
+    }
+};
+
+const img = new Image();
+img.src = currentframe(1);
+canvas.width = img.width;
+canvas.height = img.height;
+img.onload = function(){
+    context.drawImage(img , 0 ,0);
+}
+
+const updateImage = index =>{
+    img.src = currentframe(index);
+    context.drawImage(img , 0 ,0);
+    context.imageSmoothingEnabled = true;
+    context.imageSmoothingQuality = "high";
+}
+
+window.addEventListener('scroll', () => {  
+    const scrollTop = html.scrollTop;
+    const maxScrollTop = html.scrollHeight - window.innerHeight;
+    const scrollFraction = scrollTop / maxScrollTop;
+    const frameIndex = Math.min(
+      framecount - 1,
+      Math.ceil(scrollFraction * framecount)
+    );
+    
+    requestAnimationFrame(() => updateImage(frameIndex + 1))
+  });
+  
+  
+
+preloadimages();
